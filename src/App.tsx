cc08441fd0c9f243d1b16fcbc77717206facee9b
Robert Fry <85572108+RobertSDG@users.noreply.github.com>
2021-11-20 21:15:16 +0000

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ToDoList, { Item } from "./components/ToDoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialList = [
+  {
+    task: "Pick up Milk",
+    priority: 1,
+  },
+  {
+    task: "Buy Eggs",
+    priority: 2,
+  },
+  {
+    task: "Buy Bread",
+    priority: 3,
+  },
+];
+
+class App extends React.Component<{}, { items: Item[] }> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      items: initialList,
+    };
+  }
+
+  render() {
+    const { items } = this.state;
+    return (
+      <div className="App">
+        <br />
+        <ToDoList items={items} />
+      </div>
+    );
+  }
 }
 
 export default App;
